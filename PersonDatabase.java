33edf9d7b471a65cf4bc99a5c0d213e39ce3282f
NotChewy1324 <camerongarrison4@gmail.com>
2019-11-10 18:46:15 -0500

    Updated Version

--- PersonDatabase.java
+++ PersonDatabase.java
@@ -1,8 +1,8 @@
 public class PersonDatabase
 {
 	public static void main(String[] args) {
-	    
-	    //Values
+	
+	//Values for Ecoli
 	int age = 15;
 	String person = "Ecoli";
 	String passions = "Track & Field";
@@ -10,7 +10,7 @@ public class PersonDatabase
 	String gradelevel = ("9");
 	String lover = "None";
 	
-	//Reader Values
+	//Reader Values for Ecoli
 	String helpername = ("Name: ");
 	String helperage = ("Age: ");
 	String helperpassions = ("Passions: ");
@@ -18,7 +18,7 @@ public class PersonDatabase
 	String helpergradelevel = ("Grade: ");
 	String helperlover = ("GF: ");
 	
-	//Total Output Vaules
+	//Total Output Vaules for Ecoli
 	String finalname = (helpername + person);
 	String finalage = (helperage + age);
 	String finalpassions = (helperpassions + passions);
@@ -26,7 +26,7 @@ public class PersonDatabase
 	String finalgradelevel = (helpergradelevel + gradelevel);
 	String finallover = (helperlover + lover);
 	
-	//Output
+	//Output for Ecoli
 	System.out.println("Person Database Information");
 	System.out.println("___________________________");
 	System.out.println(finalname);
@@ -36,6 +36,75 @@ public class PersonDatabase
 	System.out.println(finalgradelevel);
 	System.out.println(finallover);
 	System.out.println("___________________________");
+	
+	//Values for Jack
+	int age2 = 13;
+	String person2 = "Jack";
+	String passions2 = "Soccer";
+	String school2 = ("Middle School");
+	String gradelevel2 = ("8");
+	String lover2 = "None";
+	
+	//Reader Values for Jack
+	String helpername2 = ("Name: ");
+	String helperage2 = ("Age: ");
+	String helperpassions2 = ("Passions: ");
+	String helperschool2 = ("School: ");
+	String helpergradelevel2 = ("Grade: ");
+	String helperlover2 = ("GF: ");
+	
+	//Total Output Vaules for Jack
+	String finalname2 = (helpername2 + person2);
+	String finalage2 = (helperage2 + age2);
+	String finalpassions2 = (helperpassions2 + passions2);
+	String finalschool2 = (helperschool2 + school2);
+	String finalgradelevel2 = (helpergradelevel2 + gradelevel2);
+	String finallover2 = (helperlover2 + lover2);
+	
+	//Output for Jack
+	System.out.println("___________________________");
+	System.out.println(finalname2);
+	System.out.println(finalage2);
+	System.out.println(finalpassions2);
+	System.out.println(finalschool2);
+	System.out.println(finalgradelevel2);
+	System.out.println(finallover2);
+	System.out.println("___________________________");
+	
+	//Values for Tim
+	int age3 = 12;
+	String person3 = "Tim";
+	String passions3 = "Swimming";
+	String school3 = ("Middle School");
+	String gradelevel3 = ("6");
+	String lover3 = "N/A";
+	
+	//Reader Values for Tim
+	String helpername3 = ("Name: ");
+	String helperage3 = ("Age: ");
+	String helperpassions3 = ("Passions: ");
+	String helperschool3 = ("School: ");
+	String helpergradelevel3 = ("Grade: ");
+	String helperlover3 = ("GF: ");
+	
+	//Total Output Vaules for Tim
+	String finalname3 = (helpername3 + person3);
+	String finalage3 = (helperage3 + age3);
+	String finalpassions3 = (helperpassions3 + passions3);
+	String finalschool3 = (helperschool3 + school3);
+	String finalgradelevel3 = (helpergradelevel3 + gradelevel3);
+	String finallover3 = (helperlover3 + lover3);
+	
+	//Output for Tim
+	System.out.println("___________________________");
+	System.out.println(finalname3);
+	System.out.println(finalage3);
+	System.out.println(finalpassions3);
+	System.out.println(finalschool3);
+	System.out.println(finalgradelevel3);
+	System.out.println(finallover3);
+	System.out.println("___________________________");
+	
 	}
 	
 }
