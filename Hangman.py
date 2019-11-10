627d0e9d5f1297a230a9fcd920d857e13bca5747
NotChewy1324 <camerongarrison4@gmail.com>
2019-11-10 18:30:55 -0500

    Create Hangman.py

---
+++ Hangman.py
@@ -0,0 +1,39 @@
+import random
+def hangman(word):
+    wrong = 0
+    stages = ["",
+              "__________          ",
+              "|                   ",
+              "|          |        ",
+              "|          O        ",
+              "|         /|\       ",
+              "|         / \       ",
+              "|                   ",
+              ]
+    rletters = list(word)
+    board = ["__"] * len(word)
+    win = False
+    print("Welcome to Hangman")
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Guess a letter: "
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = '$'
+        else:
+            wrong += 1
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n".join(stages[0:e]))
+        if "__" not in board:
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n".join(stages[0:wrong]))
+        print("You lose! It was {}.".format(word))
+
+wordlst = ["cat", "dog", "goat", "cpu", "ram", "iphone", "Farmer", "Hello", "World"]
+word = random.choice(wordlst[:])
+hangman(word)
