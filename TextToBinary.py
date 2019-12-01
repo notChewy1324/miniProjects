c34f04dbc08c9f1e3f435ef0acc77fb06c80f09e
NotChewy1324 <camerongarrison4@gmail.com>
2019-11-30 21:12:08 -0500

    Create TextToBinary.py

---
+++ TextToBinary.py
@@ -0,0 +1,13 @@
+import random
+while(True):
+    user = input("Type: ")
+    count = len(user)
+    for count in range(count):
+        binary = random.randint(0,1)
+        print(binary)
+        continue
+    user2 = input("Type Again? ( y or n) :")
+    if (user2 == "y"):
+        continue
+    else:
+        break
