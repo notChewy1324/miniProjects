50bbf0cdd5587a559195c0ffbeee8bb26c744941
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2020-01-10 15:41:02 -0500

    Create MessageBot.py

---
+++ MessageBot.py
@@ -0,0 +1,12 @@
+import pyautogui
+import time
+
+time.sleep(2) # Will allow you to prepare the space to write to
+
+while(True):
+    pyautogui.press("S")
+    pyautogui.press("P")
+    pyautogui.press("A")
+    pyautogui.press("M")
+    pyautogui.press("enter")
+    continue
