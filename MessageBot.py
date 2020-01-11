c1dfb0996a3f13f72a0c7479b4cb8c1bf4b2a647
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2020-01-10 22:42:30 -0500

    Updated Version

--- MessageBot.py
+++ MessageBot.py
@@ -1,9 +1,12 @@
 import pyautogui
 import time
 
+num = 0
 time.sleep(2) # Will allow you to prepare the space to write to
 
 while(True):
+    num = num + 1
+    print(num) # Displays the current number the loop
     pyautogui.press("S")
     pyautogui.press("P")
     pyautogui.press("A")
