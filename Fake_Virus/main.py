0849062f618bb8abce9eb7709516f1e2e2df106d
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2020-11-24 16:48:17 -0500

    Updated Code

--- Fake_Virus/main.py
+++ Fake_Virus/main.py
@@ -3,19 +3,19 @@
 import sys, glob
 
 code = []
-with open(sys.argv[0], 'r') as f:
-    lines = f.readlines
+with open(sys.argv[0], "r") as f:
+    y = f.readlines
 
 virus_area = False
-for line in lines:
-    if line == '# Virus Start\n':
+for x in y:
+    if x == "# Virus Start\n":
         virus_area = True
     if virus_area:
-        code.append(line)
-    if line == "# Virus End\n":
+        code.append(x)
+    if x == "# Virus End\n":
         break
 
-python_scripts = glob.glob('*.py') + glob.glob('*.pyw')
+python_scripts = glob.glob("*.py") + glob.glob("*.pyw")
 print(python_scripts)
 
 # Virus End
