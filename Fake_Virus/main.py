5b7d497d03c7b65e055b1a37febf79d47e2aa58a
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2020-11-24 16:35:19 -0500

    Filtered Out Virus Code

---
+++ Fake_Virus/main.py
@@ -0,0 +1,16 @@
+# Virus Start
+
+import sys, glob
+
+code = []
+with open(sys.argv[0], 'r') as f:
+    lines = f.readlines
+
+virus_area = False
+for line in lines:
+    if line == '# Virus Start\n':
+        virus_area = True
+    if virus_area:
+        code.append(line)
+    if line == "# Virus End\n":
+        break
