1876164f72c903e88cd3bf44a4b32efedbfef45c
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2022-03-13 11:55:55 -0400

    Update languageTranslator.py

--- languageTranslator.py
+++ languageTranslator.py
@@ -4,5 +4,17 @@ translator = Translator()
 
 txt = input("Type anything... ")
 
-usrTXT = translator.translate(f"{txt}", dest='es')
+usrTXT = translator.translate(f"{txt}", dest='es') #change the 'dest' to change the output language
 print(f"{usrTXT.origin} ({usrTXT.src}) ---> {usrTXT.text} ({usrTXT.dest})")
+
+
+sentences = [
+    "Hello World",
+    "Just setting up some code",
+    "what the dog doing",
+    "Bye World"
+]
+
+translations = translator.translate(sentences, dest="ar")
+for translation in translations: #translate many different lines at once
+    print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
