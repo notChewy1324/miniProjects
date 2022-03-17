8d4e7e87ee979d45398e6f9429cc1746c8e56c26
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2022-03-16 22:34:35 -0400

    Update plagiarismChecker.py

--- plagiarismChecker.py
+++ plagiarismChecker.py
@@ -7,14 +7,18 @@ import plotly.graph_objects as go
 from scipy.ndimage import gaussian_filter
 import requests
 
+
+
 try:
-    train_data_file = requests.get("https://raw.githubusercontent.com/notChewy1324/miniProjects/d0627c5dd3581e161a4ce9ec0cedbf48969010e2/training_data.json") #File with training data
+    train_data_file = requests.get("https://raw.githubusercontent.com/notChewy1324/miniProjects/master/training_data.json") #File with training data
 except:
     print("Failed to create an connection. Check your internet.")
     
-train_data_json = [train_data_file.json()['line1']]
+train_data_json = train_data_file.json()['TestingINFO']
+
+print(f"DEBUG INFO: {train_data_json}")
     
-train_data_json = re.sub(r"\[.*\]|\{.*\}", "", train_data_json)
+train_data_json = re.sub(r"\[.*\]|\{.*\}", "", train_data_json["line1"]) #selected a line to fix TypeError
 train_data_json = re.sub(r'[^\w\s]', "", train_data_json)
 
 n = 4
@@ -25,3 +29,24 @@ training_data = list(pad_sequence(word_tokenize(train_data_json), n,
 
 ngrams = list(everygrams(pad_sequence, max_len=n))
 print("Number of ngrams: ", len(ngrams))
+
+model = WittenBellInterpolated(n)
+model.fit([ngrams], vocabulary_text=training_data)
+print(model.vocab)
+
+
+
+try:
+    test_data_file = requests.get("https://raw.githubusercontent.com/notChewy1324/miniProjects/master/testing_data.json") #File with training data
+except:
+    print("Failed to create an connection. Check your internet.")
+    
+test_text = test_data_file.json()["line1"]
+
+test_text = re.sub(r'[^\w\s]', "", test_text)
+
+
+testing_data = list(pad_sequence(word_tokenize(test_text), n, 
+                                 pad_left=True,
+                                 left_pad_symbol="<s>"))
+print("Length of test data:", len(testing_data))
