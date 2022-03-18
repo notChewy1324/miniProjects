d9615ea8494a30c2240a6b1e9fc31bba780a0718
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2022-03-18 18:02:24 -0400

    Created GUI

--- plagiarismChecker.py
+++ plagiarismChecker.py
@@ -61,7 +61,31 @@ for i, item in enumerate(testing_data[n-1:]):
     
 
 #Gui
-
 width = 8
-height = np.ceil(len(testing_data)/width)
+height = int(len(testing_data)/width)
 print(f"Width: {width}, Height: {height}")
+
+a = np.zeros(width*height)
+a[:len(scores_np)] = scores_np
+diff = len(a) - len(scores_np)
+
+a = gaussian_filter(a, sigma=1.0)
+
+a = a.reshape(-1, width) # reshape to fit rectangle
+
+# format labels
+labels = [" ".join(testing_data[i:i+width]) for i in range(n-1, len(testing_data), width)]
+labels_individual = [x.split() for x in labels]
+labels_individual[-1] += [""]*diff
+labels = [f"{x:60.60}" for x in labels]
+
+# create heatmap
+fig = go.Figure(data=go.Heatmap(
+                z=a, x0=0, dx=1,
+                y=labels, zmin=0, zmax=1,
+                customdata=labels_individual,
+                hovertemplate='%{customdata} <br><b>Score:%{z:.3f}<extra></extra>',
+                colorscale="burg"))
+fig.update_layout({"height":height*28, "width":1000, "font":{"family":"Courier New"}})
+fig['layout']['yaxis']['autorange'] = "reversed"
+fig.show()
