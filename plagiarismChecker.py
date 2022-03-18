781c2713af652cb81b41de6e89807b1495bd2214
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2022-03-17 21:59:08 -0400

    Update plagiarismChecker.py

--- plagiarismChecker.py
+++ plagiarismChecker.py
@@ -1,4 +1,6 @@
+from asyncore import read
 import re
+from urllib import request
 from nltk import ngrams, pad_sequence, everygrams
 from nltk.tokenize import word_tokenize
 from nltk.lm import MLE, WittenBellInterpolated
@@ -8,13 +10,14 @@ from scipy.ndimage import gaussian_filter
 import requests
 
 
+requestFile = "https://raw.githubusercontent.com/notChewy1324/miniProjects/65541e1b32abb557d8cec176cc838405080acc6a/plagiarismData.json"
 
 try:
-    train_data_file = requests.get("https://raw.githubusercontent.com/notChewy1324/miniProjects/master/training_data.json") #File with training data
+    train_data_file = requests.get(f"{requestFile}") #File with training data
 except:
     print("Failed to create an connection. Check your internet.")
     
-train_data_json = train_data_file.json()['TestingINFO']
+train_data_json = train_data_file.json()['TrainingINFO']
 
 print(f"DEBUG INFO: {train_data_json}")
     
@@ -23,7 +26,7 @@ train_data_json = re.sub(r'[^\w\s]', "", train_data_json)
 
 n = 4
 
-training_data = list(pad_sequence(word_tokenize(train_data_json), n, 
+training_data = list(pad_sequence(word_tokenize(train_data_json[0], train_data_json[1]), n, #Work on make the data_json file iterable
                                   pad_left=True, 
                                   left_pad_symbol="<s>"))
 
@@ -37,11 +40,11 @@ print(model.vocab)
 
 
 try:
-    test_data_file = requests.get("https://raw.githubusercontent.com/notChewy1324/miniProjects/master/testing_data.json") #File with training data
+    test_data_file = requests.get(f"{requestFile}") #File with testing data
 except:
     print("Failed to create an connection. Check your internet.")
     
-test_text = test_data_file.json()["line1"]
+test_text = test_data_file.json()["TestingINFO"]
 
 test_text = re.sub(r'[^\w\s]', "", test_text)
 
