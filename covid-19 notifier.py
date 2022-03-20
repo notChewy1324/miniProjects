429cf0da3909e7b77e0dabe1e599703d8d4857c6
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2022-03-19 20:57:32 -0400

    Cleaned Up Code

--- covid-19 notifier.py
+++ covid-19 notifier.py
@@ -4,27 +4,26 @@ from socket import timeout
 import time
 import requests
 
-covidData = None
 covidTime = 5 #Change this number to change the request time: formated in seconds
+
 try: 
-    covidData = requests.get("https://corona-rest-api.herokuapp.com/Api/usa/")
+    covidAPI = requests.get("https://corona-rest-api.herokuapp.com/Api/usa/")
 except:
     print("Failed to create an connection. Check your internet.")
     
 
-if (covidData != None):
-    data = covidData.json()['Success']
+covidData = covidAPI.json()['Success']
     
-    print(f"Refreshes every {covidTime} seconds.")
+print(f"Refreshes every {covidTime} seconds.")
+
+while True:
     
-    while True:
-        
-            covidDate = "COVID-19 Stats on {}".format(datetime.today().strftime('%Y-%m-%d-%H:%M:%S'))
+    covidDate = "COVID-19 Stats on {}".format(datetime.today().strftime('%Y-%m-%d-%H:%M:%S'))
             
-            totalcases = data['cases'],
-            todaycases = data['todayCases'],
-            todaydeaths = data['todayDeaths'],
-            active = data["active"]
+    totalcases = covidData['cases'],
+    todaycases = covidData['todayCases'],
+    todaydeaths = covidData['todayDeaths'],
+    active = covidData["active"]
     
-            time.sleep(covidTime)
-            print(f"{covidDate} \nTotal Cases Right Now: {totalcases} \nCase Count Today: {todaycases} \nDeaths Today: {todaydeaths} \n\n")
+    time.sleep(covidTime)
+    print(f"{covidDate} \nTotal Cases Right Now: {totalcases} \nCase Count Today: {todaycases} \nDeaths Today: {todaydeaths} \n\n")
