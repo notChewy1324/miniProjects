7c6086299f88de645c7c4dc3fcd7c034b58f4d40
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2022-03-23 23:02:51 -0400

    removed testing data

--- plagiarismChecker.py
+++ plagiarismChecker.py
@@ -14,28 +14,8 @@ import json
 
 requestFile = "https://raw.githubusercontent.com/notChewy1324/miniProjects/master/plagiarismData.json"
 
-try:
-    train_data_file = requests.get(f"{requestFile}") #File with training data
-except:
-    print("Failed to create an connection. Check your internet.")
-    
-train_data_json = train_data_file.json()['TrainingINFO']
-    
-train_data_json = re.sub(r"\[.*\]|\{.*\}", "", str(train_data_json))
-train_data_json = re.sub(r'[^\w\s]', "", train_data_json)
-
 n = 4
-
-training_data = str(train_data_json)
-
-#ngrams = list(training_data)
-#print("Number of ngrams: ", len(ngrams))
-
 model = WittenBellInterpolated(n)
-#model.fit([str(ngrams)], vocabulary_text=training_data)
-print(model.vocab)
-
-
 
 try:
     test_data_file = requests.get(f"{requestFile}") #File with testing data
