f8cabef4ca4b64769ace5107c0c6168411fa215f
Cameron Garrison <40218645+notChewy1324@users.noreply.github.com>
2022-03-29 22:19:00 -0400

    Update snakeGame.py

--- snakeGame.py
+++ snakeGame.py
@@ -6,7 +6,7 @@ dis = pygame.display.set_mode((800,600))
 pygame.display.update()
 pygame.display.set_caption('Snake game by notchewy1324')
 
-white = (255, 255, 255)
+white = (0, 255, 0) #G
 black = (0, 0, 0)
 blue=(0,0,255)
 red=(255,0,0)
@@ -47,5 +47,3 @@ while not game_over:
 
 pygame.quit()
 quit()
-
-
